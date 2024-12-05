06f14004359c1659b54d7aec21b49006dda8bf5e
Dmitry Ponyatov <dponyatov@gmail.com>
2024-12-05 21:36:20 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- horizon.mk
+++ horizon.mk
@@ -8,11 +8,14 @@ BOARD = boards/$(HZ).py
 C += src/jsinteractive.c
 H += 
 S += targetlibs/stm32f4/lib/startup_stm32f40_41xxx.s
+P += boards/$(HZ).py
 
 .PHONY: $(JSON)
 $(JSON): $(BOARD)
 	BOARD=$(HZ) DEBUG=1 make boardjson
 
 # merge
-MERGE += CMakePresets.json CMakeLists.txt apt.Debian
-MERGE += $(C) $(H) $(S)
+MERGE += horizon.mk Makefile README.md apt.Debian
+MERGE += Doxyfile .clang-format .gitignore
+MERGE += CMakePresets.json CMakeLists.txt cmake
+MERGE += $(C) $(H) $(S) $(P)
